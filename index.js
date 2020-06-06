535279f00380c1618b7fd9f2f9d5c573c1dc5ebf
sashmlv <sashmlv@yandex.ru>
2020-06-06 11:11:08 +0300

    Remove inappropriate functions.

--- index.js
+++ index.js
@@ -1,26 +1,12 @@
 'use strict';
 
-const ch = require( 'chalk' ),
-   fs = require( 'fs' ),
-   util = require( 'util' ),
-   path = require( 'path' ),
-   access = util.promisify( fs.access );
+const ch = require( 'chalk' );
 
 /**
  * Constructor
  * @constructor
  **/
 class Maintenance {
-
-	/**
-	 * Get type of variable
-	 * @param { * } - any value
-	 * @return { string } class
-	 **/
-	getClass( obj ) {
-
-		return {}.toString.call( obj ).slice( 8, -1 ).toLowerCase();
-	}
 
    /**
     * Log, mainly for development
@@ -36,39 +22,9 @@ class Maintenance {
 
          args.length && ( args = args.map( v => clb( v )));
          ! args.length && ( str = clb( str ));
-      }
+      };
 
       return console.log( str, ...args );
-   };
-
-   /**
-    * Check file exists
-    * @param  {string} filePath
-    * @param  {boolean} tryFix
-    * @return {boolean} Return file exists
-    **/
-   async exists( filePath, tryFix ) {
-
-      try {
-
-         if( tryFix ){
-
-            filePath = path.resolve( filePath );
-         }
-
-         await access( filePath, fs.constants.F_OK );
-
-         return true;
-      }
-      catch( e ) {
-
-         if( e.code === 'ENOENT' ) {
-
-            return false;
-         }
-
-         throw e;
-      };
    };
 };
 
